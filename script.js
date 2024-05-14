ca2c5f009ce333668676ff829306a05b852c7b7f
KuldeepGupta <kuldeepswnkumar@gmail.com>
2024-05-14 23:37:47 +0530

    frontend add

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+// const circularProgress = document.querySelectorAll(".circular-progress");
+
+
+
+// let displayList = [];
+// var list = document.getElementById("myTable");
+// displayList.forEach((item) => {
+//     let td = document.createElement("td");
+//     td.innerText = item;
+//     list.appendChild(td);
+// });
+
+
+
+
+
+
+
+
+
+
+
